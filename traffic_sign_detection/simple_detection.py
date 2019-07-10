70a8fbd2585f9097779e5c1ecf1e58f39816af31
MiguelMendozaG <miguel.mendoza.11.1987@gmail.com>
2019-07-10 03:18:05 +0000

    first working version of simple detection

--- traffic_sign_detection/simple_detection.py
+++ traffic_sign_detection/simple_detection.py
@@ -1,6 +1,6 @@
 import argparse
 import time
-
+import re
 import numpy as np
 import os
 import datetime
@@ -10,18 +10,17 @@ from PIL import Image
 
 model = "road_signs_quantized_edgetpu.tflite"
 label = 'road_sign_labels.txt'
-image_path = "data/test/2019-04-16-095558.jpg"
+image_path = "data/test/2019-04-16-100317.jpg"
+
+with open(label, 'r') as f:
+	pairs = (l.strip().split(maxsplit=1) for l in f.readlines())
+	labels = dict((int(k), v) for k, v in pairs) 
 
 engine = edgetpu.detection.engine.DetectionEngine(model)
 
-
-def load_image_into_numpy_array(image):
-    (im_width, im_height) = image.size
-    return np.array(image.getdata()).reshape(
-        (im_height, im_width, 3)).astype(np.uint8)
-
-
 image = Image.open(image_path)
-img_pil = Image.fromarray(image)
-results = engine.DetectWithImage(img_pil, threshold=min_confidence, keep_aspect_ratio=True,
-                                   relative_coord=False, top_k=5)
+results = engine.DetectWithImage(image, threshold = 0.20, top_k = 6)
+
+for result in results:
+	print(labels[result.label_id], result.score*100)
+
